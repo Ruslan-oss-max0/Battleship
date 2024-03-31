86dab9d9b77087f054c805ebc0710c7159ff1940
Ruslan <111634386+Ruslan-oss-max0@users.noreply.github.com>
2024-03-31 16:14:17 +0000

    Added the view object to the game

--- battleship.js
+++ battleship.js
@@ -1,37 +1,30 @@
-let randomLoc = Math.floor(Math.random() * 5);
-let location1 = randomLoc;
-let location2 = location1 + 1;
-let location3 = location2 + 1;
+let view = {
+  displayMessage(msg) {
+    let messageArea = document.getElementById('messageArea');
+    messageArea.innerHTML = msg;
+  },
+  displaHit(location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute('class', 'hit');
+  },
+  displayMiss(location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute('class', 'miss');
+  },
+}
 
-let guess;
-let hits = 0;
-let guesses = 0;
+let ships = [
+  {
+    locations: ['10', '20', '30'],
+    hits: ['', '', ''],
+  },
+  {
+    locations: ['32', '33', '34'],
+    hits: ['', '', ''],
+  },
+  {
+    locations: ['63', '64', '65'],
+    hits: ['', '', 'hit'],
+  }
+];
 
-let isSunk = false;
-
-// while (isSunk == false) {
-//   guess = +prompt("Enter a number between 0 and 6: ");
-//   if (guess < 0 || guess > 6) {
-//     alert("Choose the numbers between 0 and 6");
-//   } else {
-//     guesses += 1;
-
-//     if (guess === location1 || guess === location2 || guess === location3) {
-//       alert("HIT");
-//       hits += 1;
-
-//       if (hits === 3) {
-//         isSunk = true;
-//         alert("You sank a buttleship");
-//       }
-//     } else {
-//       alert("MISS");
-//     }
-//   }
-// }
-
-let stats = `You took ${guesses} guesses to sink the battleship, which means your shooting accuracy was ${
-  3 / guesses
-}`;
-
-alert(stats);
